5b66cf0ad0317dfb16f384c0dde347f3bf78c45f
Konicz, Sebastian P <Sebastian.Konicz@nielsen.com>
2020-12-08 21:15:38 +0100

    Finished script for scrapping players sites links with stats

--- src/Fantasy_01_scrap_players_sites.py
+++ src/Fantasy_01_scrap_players_sites.py
@@ -1,7 +1,5 @@
 from bs4 import BeautifulSoup
-import os
-import re
-import requests
+from pathlib import Path
 import pandas as pd
 import time
 from selenium import webdriver
@@ -15,40 +13,61 @@ def main():
     # start time of function
     start_time = time.time()
 
-    # Getti
-    main_page = requests.get('https://fantasy.ekstraklasa.org/stats')
-    html_main_page = BeautifulSoup(main_page.content, 'html.parser')
-    links_years = html_main_page.find_all("tbody")
+    # project directory
+    project_dir = str(Path(__file__).resolve().parents[1])
 
+    # gettig the main page with fantasy soccer stat
     driver = webdriver.Chrome(PATH)
     driver.get('https://fantasy.ekstraklasa.org/stats')
 
     site_list =[]
+    # looping throug pagination (only 28 sites - gave 30 to be sure)
     for i in range(30):
+        # getting the site
+        html = driver.page_source
+        site = BeautifulSoup(html, 'html.parser')
+        # adding site to site_list
+        site_list.append(site)
+        # getting the button for the next site
         next_button = driver.find_element_by_link_text('Następny')
-        site_list.append(next_button)
-        print(next_button)
+        # going to the next site
         next_button.click()
 
-    print(site_list)
+    # empty lists for values
+    players_list = []
+    links_list = []
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
+    # looping thgrou sites
+    for site in site_list:
+        # getting rows form table
+        table_body = site.find_all('td', class_="sorting_1")
+        for td in table_body:
+            # getting a tag with player's name and link
+            a = td.find("a")
+            # player name
+            player = a.text
+            # link to player's stats
+            link = 'fantasy.ekstraklasa.org' + a['href']
+            # appending lists
+            players_list.append(player)
+            links_list.append(link)
 
-    table_body = soup.find_all('td', class_="sorting_1")
-    # table_links = table_body.find_all("a")
-    # player_link = table_body.get('href')
+    # zipping lists
+    data_tuples = list(zip(players_list, links_list))
 
-    for td in table_body:
-        a = td.find_all("a")
-        print(a)
+    # creating dataframe
+    players_links_df = pd.DataFrame(data_tuples, columns=['Player', 'Link'])
 
+    # saving dataframe
+    players_links_df.to_csv(project_dir + r'\data\raw\Players_links.csv', index=False, encoding='UTF-8')
 
-    print(table_body)
-    # print(player_link)
-
-
+    # shutting down selenium driver
     driver.quit()
+
+    # end time of program + duration
+    end_time = time.time()
+    execution_time = int(end_time - start_time)
+    print('\n', 'exectution time = ', execution_time, 'sec')
 
 if __name__ == "__main__":
     main()
