c86ae800a98ecc97e6700b37cdd737d8105dad0e
sebastian-konicz <sebastian.konicz@gmail.com>
2020-12-13 19:38:08 +0100

    Added script scraping stats for players form first league

--- src/first_league/first_league_01_scrap_players_sites.py
+++ src/first_league/first_league_01_scrap_players_sites.py
@@ -54,7 +54,8 @@ def main():
             # player name
             player = a.text
             # link to player's stats
-            link = 'https://fantasy.1liga.org/pl' + a['href']
+            link = 'https://fantasy.1liga.org' + a['href']
+            print(link)
             # appending lists
             players_list.append(player)
             links_list.append(link)
