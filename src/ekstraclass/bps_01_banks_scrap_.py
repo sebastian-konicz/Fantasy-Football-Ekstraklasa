cb56de2db340558b7b1ada4e395278f1cb18745d
unknown <sebastian.konicz@gmail.com>
2022-10-20 11:34:33 +0200

    data for bps project

--- src/ekstraclass/bps_01_banks_scrap_.py
+++ src/ekstraclass/bps_01_banks_scrap_.py
@@ -15,9 +15,10 @@ def main():
 
     # output files
     # players_links_path = r'\data\raw\ekstraclass\01_players_links_{date}.csv'.format(date=var.time_stamp)
-    gminy_names_path = r'\data\bps\raw\gminy_names_short.csv'
-    # gminy_names_path = r'\data\bps\raw\gminy_names.csv'
+    # gminy_names_path = r'\data\bps\raw\gminy_names_short.csv'
+    gminy_names_path = r'\data\bps\raw\gminy_names.csv'
     gminy_file_path = r'\data\bps\final\gminy_names_final.csv'
+    final_file_path = r'\data\bps\final\point_adress_all.csv'
 
     # start time of function
     start_time = time.time()
@@ -26,10 +27,13 @@ def main():
     path = var.project_dir
 
     # gminy dataframe
-    gm = pd.read_csv(path + gminy_names_path)
+    gm = pd.read_csv(path + gminy_names_path, sep=';')
 
     # reshaping and cleaning df
+    gm['pow_teryt'] = gm['pow_teryt'].apply(lambda x: str(x).zfill(4))
     gm['gmi_name'] = gm['gmi_name'].apply(lambda x: str(x).lower().replace('gmina ', ''))
+    gm['pow_name'] = gm['pow_name'].apply(lambda x: str(x).lower().replace('powiat ', ''))
+
     print(gm.head())
 
     # saving dataframe
@@ -37,14 +41,18 @@ def main():
 
     # dataframe to lis
     gm_list = gm['gmi_name'].to_list()
+    teryt_list = gm['pow_teryt'].to_list()
 
-    print(gm_list)
+    gm_dict = dict(zip(gm_list, teryt_list))
 
     # bps site
     bps_www = 'https://mojbank.pl/znajdz-placowke'
 
-    for gmina in gm_list:
-        print(gmina)
+    df_list = []
+
+    for gmina, teryt in gm_dict.items():
+        print(gmina, teryt)
+
         # gettig the main page with fantasy soccer stat
         driver = webdriver.Chrome(var.chrome_driver)
         driver.get(bps_www)
@@ -59,109 +67,99 @@ def main():
         inputElement.send_keys(Keys.ARROW_UP)
         time.sleep(0.5)
 
-        # prnit('dupa')
-
-        # driver.execute_script("""
-        # var input = document.getElementById("cruzmap-location");
-        # input.addEventListener("keyup", function(event)
-        # {if (event.keyCode === 13)
-        #     {event.preventDefault();
-        #     document.getElementById("cruzmap-search").click();
-        # }
-        # });
-        # """)
-
-        # cruz_map_location = driver.find_element("id", "cruzmap-location")
-        # driver.implicitly_wait(100)
-        # cruz_map_location.send_keys(Keys.ARROW_UP)
-        # print(cruz_map_location)
-        #
-        # # getting the button for the next site
-        # next_button = driver.find_element("id", 'cruzmap-search')
-        # print(next_button)
-        # # going to the next site
-        # next_button.click()
-
-        # getting the site
-        html = driver.page_source
-        site = BeautifulSoup(html, 'html.parser')
-
-        # print(site)
-
-        # print(site)
+        # getting div with points data
         all_divs = driver.find_element("id", 'cruzmap-points')
-        print(all_divs)
         div_html = all_divs.get_attribute('innerHTML')
-        print(div_html)
+        # print(div_html)
 
-        # all_divs_2 = site.find_all('div', id="cruzmap-points")
-        # print(all_divs_2)
-
+        # parsing div_html
         point_info = BeautifulSoup(div_html, 'html.parser')
-
         all_points = point_info.find_all('div', class_="point-info")
-        print(all_points)
 
+        points_length = len(all_points)
 
+        # empty listst
+        point_id_list = []
+        point_name_list = []
+        point_street_list = []
+        point_address_list = []
+        point_teryt_list = []
+        point_gmina_list = []
+        point_phone_list = []
+        point_url_list = []
 
-        #
-        # points_length = len(all_divs)
-        #
-        # if points_length == 0:
-        #     print('brak punktow w okolicy')
-        #     pass
-        # else:
-        #     print(all_divs)
+        if points_length == 0:
+            print('brak punktow w okolicy')
+            pass
+        else:
+            for point in all_points:
+                point_id = point.get('id')
+                # point_teryt = teryt
+                point_name = point.find('div', class_='point-name').text
+                point_street = point.find('div', class_='point-street').text
+                point_address = point.find('div', class_='point-address').text
+                point_teryt = teryt
+                point_gmina = gmina
+                try:
+                    point_phone = point.find('div', class_='point-phone').text
+                except AttributeError:
+                    point_phone = ''
+                    print("There's no telephone value")
+                try:
+                    point_url = point.find('div', class_='point-url').text
+                except AttributeError:
+                    point_url = ''
+                    print("There's no url value")
+
+                print(point_name)
+
+                point_id_list.append(point_id)
+                point_name_list.append(point_name)
+                point_street_list.append(point_street)
+                point_address_list.append(point_address)
+                point_teryt_list.append(point_teryt)
+                point_gmina_list.append(point_gmina)
+                point_phone_list.append(point_phone)
+                point_url_list.append(point_url)
+
+        data_tuples = list(zip(point_id_list, point_name_list,
+                               point_street_list, point_address_list,
+                               point_teryt_list, point_gmina_list,
+                               point_phone_list, point_url_list))
+
+        point_data = pd.DataFrame(data_tuples, columns=["id", "name",
+                                                        "street", "adress",
+                                                        "pow_teryt", "gmina_town",
+                                                        "phone", "url"])
+
+        df_list.append(point_data)
+        # saving dataframe
+        points_interim_pat = r'\data\bps\interim\points_{gmina}.csv'.format(gmina=gmina)
+        point_data.to_csv(path + points_interim_pat, index=False)
 
         # shutting down selenium driver
         driver.quit()
 
-    # map_html = cruz_map.get_attribute('innerHTML')
-    #
-    # print(map_html)
+    df_final = pd.concat(df_list)
 
-    # point = driver.find_element_by_class_name('point_info')
-    # points = driver.find_elements_by_class_name('point_info')
-    #
-    # print(point)
-    # print(points)
+    df_final.drop_duplicates(subset=['id'], keep='first', inplace=True)
+    df_final.reset_index(inplace=True)
 
-    # html = driver.page_source
-    # site = BeautifulSoup(html, 'html.parser')
-    # print(site)
-    # all_divs = site.find_all('div', class_="point_info")
-    # print(all_divs)
+    df_final = df_final.merge(gm,
+                              how='left',
+                              left_on=["pow_teryt", "gmina_town"],
+                              right_on=["pow_teryt", "gmi_name"])
 
-    #
-    # # empty lists for values
-    # players_list = []
-    # links_list = []
-    #
-    # # looping thgrou sites
-    # for site in site_list:
-    #     # getting rows form table
-    #     table_body = site.find_all('td', class_="sorting_1")
-    #     for td in table_body:
-    #         # getting a tag with player's name and link
-    #         a = td.find("a")
-    #         # player name
-    #         player = a.text
-    #         # link to player's stats
-    #         link = 'https://fantasy.ekstraklasa.org' + a['href']
-    #         # appending lists
-    #         players_list.append(player)
-    #         links_list.append(link)
-    #
-    # # zipping lists
-    # data_tuples = list(zip(players_list, links_list))
-    #
-    # # creating dataframe
-    # players_links_df = pd.DataFrame(data_tuples, columns=['player', 'link'])
-    #
-    # # saving dataframe
-    # players_links_df.to_csv(path + players_links_path, index=False, encoding='UTF-8')
+    df_final = df_final[["id", "name", "street", "adress",
+                        "pow_teryt", "pow_name", "gmina_town",
+                        "phone", "url"]]
 
+    print(df_final.head())
+    print("długosc całego dataframu")
+    print(len(df_final.index))
 
+    # saving dataframe
+    df_final.to_csv(path + final_file_path, index=False)
 
     # end time of program + duration
     end_time = time.time()
